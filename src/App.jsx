8fe9c06ccd16e7e31d45059e9e5d9f763c7efeb0
alex <alex_fernandezferreira@iescarlesvallbona.com>
2022-03-08 17:41:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const obtenerLocalStorage = () => {
+      const pacientesLocalStorage =
+        JSON.parse(localStorage.getItem("pacientes")) ?? [];
+
+      setPacientes(pacientesLocalStorage);
+    };
+
+    obtenerLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    console.log("Eliminando paciente", id);
+
+    const pacientesActulizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
+
+    setPacientes(pacientesActulizados);
+  };
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
